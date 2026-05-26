2bbbd1040e38ead53263931a9691eb2fabcf21d3
Matias <juromatias336@gmail.com>
2026-05-25 19:16:40 -0500

    subiendo archivos

---
+++ po web/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+import Auth from './pages/Auth';
+import Admin from './pages/Admin';
+import Checkout from './pages/Checkout';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
